181eb6e510150289cbe0d70b617f664e3f532340
kate <katejohn967@gmail.com>
2022-06-04 10:13:22 +0100

    fixed server bug

--- client-side/main.js
+++ client-side/main.js
@@ -4,9 +4,10 @@ async function apiRequest() {
   const rapperName = document.querySelector("input").value;
   try {
     const response = await fetch(
-      `https://simple-rapper-api.herokuapp.com/api/${rapperName}`
+      `https://raps-api-100devs.herokuapp.com/api/${rapperName}`
     );
     const data = await response.json();
+
     console.log(data);
     document.querySelector("h2").innerText = data.birthName;
   } catch (error) {
--- server.js
+++ server.js
@@ -1,25 +1,46 @@
-const { request } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8000;
 const cors = require("cors");
 
 app.use(cors());
-const rappers = {
+
+let rappers = {
   "21 savage": {
     age: 29,
     birthName: "Sheyaa Bin Abraham-Joseph",
     birthLocation: "London, England",
+    origin: "Atlanta, Georgia",
+    genre: "hip hop, trap, rap, horrorcore",
+    occupation: "rapper, songwriter, record producer",
+    yearsActive: "2013-present",
+    labels: "Epic, Slaughter Gang",
+    children: 3,
   },
   "chance the rapper": {
     age: 29,
     birthName: "Chancelor Bennett",
-    birthLocation: "Chicago, Illinois",
+    birthdate: "April 16, 1993",
+    birthLocation: "London, England",
+    origin: "Chicago, Illinois",
+    genre: "hip hop, alternative hip hop, r & b",
+    occupation:
+      "rapper, singer, song writer, record producer, activist, actor, philanthropist",
+    yearsActive: "2011-present",
+    labels: "none",
+    children: 0,
   },
   unknown: {
     age: 0,
     birthName: "unknown",
+    birthdate: "unknown",
     birthLocation: "unknown",
+    origin: "unknown",
+    genre: "unknown",
+    occupation: "unknown",
+    yearsActive: "unknown",
+    labels: "unknown",
+    children: "unknown",
   },
 };
 
@@ -38,5 +59,5 @@ app.get("/api/:name", (req, res) => {
 });
 //try using heroku's port, if its not possible then use ours
 app.listen(process.env.PORT || PORT, () => {
-  console.log(`The server is now running on port ${PORT}`);
+  console.log(`Server is now running on port ${PORT}`);
 });
